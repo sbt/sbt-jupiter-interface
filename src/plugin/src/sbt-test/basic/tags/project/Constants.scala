6e062752e850a7da0687ff8fd4b23c00a9aecc89
Michael Aichler <m.aichler@mediacluster.de>
2019-09-25 11:30:44 +0200

    Add handling of quotes to parameter values and related test cases (#38)

--- src/plugin/src/sbt-test/basic/tags/project/Constants.scala
+++
@@ -1,9 +0,0 @@
-// because it seems impossible in a set command to put a string literal in a scripted test
-object Constants {
-  val IncludeFast = "--include-tags=fast"
-  val IncludeSlow = "--include-tags=slow"
-  val IncludeFastAndSlow = "--include-tags=fast,slow"
-  val ExcludeFast = "--exclude-tags=fast"
-  val ExcludeSlow = "--exclude-tags=slow"
-  val ExcludeFastAndSlow = "--exclude-tags=fast,slow"
-}
