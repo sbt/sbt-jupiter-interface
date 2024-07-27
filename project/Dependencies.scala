ffd8405aaa0d7aefda0bae9bfeb9592276e97aa4
eugene yokota <eed3si9n@gmail.com>
2024-07-27 18:41:03 -0400

    Refactor to version strings (#82)
    To make sure that Scala Steward etc will atomically change the JUnit module versions this refactors the version strings into functions
    such that the platform version and the vintage version is calculated off of the jupiter version.

---
+++ project/Dependencies.scala
@@ -0,0 +1,20 @@
+import sbt.*
+
+object Dependencies {
+  val junitJupiterVer = "5.9.3"
+  // based on https://github.com/junit-team/junit5/releases/tag/r5.10.3 etc
+  // The platform version seems to be "1." of the corresponding JUnit 5.x version.
+  def junitPlatformVer = junitJupiterVer.replaceFirst("""^5\.""", "1.")
+  def junitVintageVer = junitJupiterVer
+  val testInterfaceVer = "1.0"
+
+  val junitPlatformLauncher = "org.junit.platform" % "junit-platform-launcher" % junitPlatformVer
+  val junitJupiterEngine = "org.junit.jupiter" % "junit-jupiter-engine" % junitJupiterVer
+  val testInterface = "org.scala-sbt" % "test-interface" % testInterfaceVer
+  val junitJupiterParams = "org.junit.jupiter" % "junit-jupiter-params" % junitJupiterVer
+  val junitVintageEngine = "org.junit.vintage" % "junit-vintage-engine" % junitVintageVer
+  val hamcrestLibrary = "org.hamcrest" % "hamcrest-library" % "1.3"
+  val mockitoCore = "org.mockito" % "mockito-core" % "2.23.4"
+  val junit4Interface = "com.github.sbt" % "junit-interface" % "0.13.3"
+  val junit4 = "junit" % "junit" % "4.13.2"
+}
