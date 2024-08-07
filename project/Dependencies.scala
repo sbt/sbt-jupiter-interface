1e7a44dfa3bf65fafce1657efa31ea68c5246f2e
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-08-07 19:29:46 +0200

    Update hamcrest-library to 3.0 (#93)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
   val testInterface = "org.scala-sbt" % "test-interface" % testInterfaceVer
   val junitJupiterParams = "org.junit.jupiter" % "junit-jupiter-params" % junitJupiterVer
   val junitVintageEngine = "org.junit.vintage" % "junit-vintage-engine" % junitVintageVer
-  val hamcrestLibrary = "org.hamcrest" % "hamcrest-library" % "2.2"
+  val hamcrestLibrary = "org.hamcrest" % "hamcrest-library" % "3.0"
   val mockitoCore = "org.mockito" % "mockito-core" % "4.11.0"
   val junit4Interface = "com.github.sbt" % "junit-interface" % "0.13.3"
   val junit4 = "junit" % "junit" % "4.13.2"
