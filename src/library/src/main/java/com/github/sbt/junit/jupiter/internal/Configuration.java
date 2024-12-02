sbt/sbt-jupiter-interface

commit e94d38edc244cc90a673f6b8cbf59f0c13464a40

re-include class names in reported tests (#126)
This amends changes from #87, by removing the redundant ".skip(1)",
and also reverts the changes done to the corresponding unit test.

src/library/src/main/java/com/github/sbt/junit/jupiter/internal/Configuration.java
@@ -317,11 +317,7 @@ public class Configuration {
               .reduce((first, last) -> last)
               .orElse(null);
 
-      return path.stream()
-          .skip(1)
-          .map(this::toName)
-          .filter(Objects::nonNull)
-          .collect(Collectors.joining());
+      return path.stream().map(this::toName).filter(Objects::nonNull).collect(Collectors.joining());
     }
 
     private List<TestIdentifier> getPath(TestPlan testPlan, TestIdentifier identifier) {
@@ -390,7 +386,7 @@ public class Configuration {
           name = colorTheme.container().format(":" + segment.getValue());
           break;
         default:
-          name = segment.getValue();
+          name = null;
           break;
       }
 
@@ -436,7 +432,7 @@ public class Configuration {
         }
       }
 
-      return "/" + identifier.getDisplayName();
+      return null;
     }
 
     /*

src/library/src/test/java/com/github/sbt/junit/jupiter/internal/listeners/FlatPrintingTestListenerFormatterTest.java
@@ -29,18 +29,22 @@ public class FlatPrintingTestListenerFormatterTest {
     // @formatter:off
 
     return new Object[][] {
-      {"jupiter.samples.NestedTests", "testOfFirstNestedClass", "$First#{1}()"},
-      {"jupiter.samples.RepeatedTests", "repeatedTest", "#{1}():#1"},
+      {"jupiter.samples.NestedTests", "testOfFirstNestedClass", "{0}$First#{1}()"},
+      {"jupiter.samples.RepeatedTests", "repeatedTest", "{0}#{1}():#1"},
       {
         "jupiter.samples.RepeatedTests",
         "repeatedTestWithRepetitionInfo",
-        "#{1}(org.junit.jupiter.api.RepetitionInfo):#1"
+        "{0}#{1}(org.junit.jupiter.api.RepetitionInfo):#1"
       },
-      {"jupiter.samples.SimpleTests", "firstTestMethod", "#{1}()"},
-      {"jupiter.samples.SimpleTests", "testWithParameter", "#{1}(org.junit.jupiter.api.TestInfo)"},
-      {"jupiter.samples.VintageTests", "vintageTestMethod", "#{1}"},
-      {"jupiter.samples.VintageEnclosedTests", "testMethod", "$NestedTest#{1}"},
-      {"jupiter.samples.VintageParameterizedTests", "testParameters", "#{1}[A-65]"},
+      {"jupiter.samples.SimpleTests", "firstTestMethod", "{0}#{1}()"},
+      {
+        "jupiter.samples.SimpleTests",
+        "testWithParameter",
+        "{0}#{1}(org.junit.jupiter.api.TestInfo)"
+      },
+      {"jupiter.samples.VintageTests", "vintageTestMethod", "{0}#{1}"},
+      {"jupiter.samples.VintageEnclosedTests", "testMethod", "{0}$NestedTest#{1}"},
+      {"jupiter.samples.VintageParameterizedTests", "testParameters", "{0}#{1}[A-65]"},
       {"jupiter.samples.SuiteTest", "firstTestMethod", "jupiter.samples.SimpleTests#{1}()"},
       {"jupiter.samples.SuiteTest", "vintageTestMethod", "jupiter.samples.VintageTests#{1}"}
     };

commit 02e4226125ffb0754acc503079788274d9b291af

Fix handling for unknown JUnit segment types (#128)
Necessary to get sbt-jupiter-interface working with Kotlin's Kotest https://github.com/com-lihaoyi/mill/pull/4048

Rather than skipping the entire unknown segment types, we filter out `"suite"` segments explicitly (which appears to be what we actually need to do, e.g. in https://github.com/sbt/sbt-jupiter-interface/blob/43918484453f25ae8e852fae48d54d84be77e0fe/src/library/src/main/java/com/github/sbt/junit/jupiter/internal/event/TaskName.java#L123) and leave unknown segment types as space-separated. We perform a final `.trim` on the generated string to ensure any leading spaces are removed, since when generating the strings for each segment we do not know whether it will be the leading segment or not

This appears to have been broken in https://github.com/sbt/sbt-jupiter-interface/pull/126/files, where returning `null` caused the entire test identifier to not render

It's not entirely clear to me why we filter out the `suite`  segments to begin with, but given that we do this seems like the better way to do it

src/library/src/main/java/com/github/sbt/junit/jupiter/internal/Configuration.java
@@ -317,7 +317,11 @@ public class Configuration {
               .reduce((first, last) -> last)
               .orElse(null);
 
-      return path.stream().map(this::toName).filter(Objects::nonNull).collect(Collectors.joining());
+      return path.stream()
+          .map(this::toName)
+          .filter(Objects::nonNull)
+          .collect(Collectors.joining())
+          .trim();
     }
 
     private List<TestIdentifier> getPath(TestPlan testPlan, TestIdentifier identifier) {
@@ -325,7 +329,10 @@ public class Configuration {
       List<TestIdentifier> result = new ArrayList<>();
 
       do {
-        if (identifier.getSource().isPresent()) {
+        // If there is only one segment, do not filter it out even
+        // if the source is not present, since we need to show something
+        boolean isOnlySegment = (result.isEmpty());
+        if (identifier.getSource().isPresent() || isOnlySegment) {
           result.add(identifier);
         }
         identifier = testPlan.getParent(identifier).orElse(null);
@@ -385,8 +392,11 @@ public class Configuration {
         case "test-template-invocation":
           name = colorTheme.container().format(":" + segment.getValue());
           break;
-        default:
+        case "suite": // Don't show junit5 suite as part of name
           name = null;
+          break;
+        default:
+          name = " " + segment.getValue();
           break;
       }
 