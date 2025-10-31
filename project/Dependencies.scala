a2c5605577be8369652a756129190498816ace4b
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-11-01 00:37:15 +0100

    Update junit-jupiter-engine, ... to 6.0.1 (#203)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt.*
 
 object Dependencies {
-  val junitVersion = "6.0.0"
+  val junitVersion = "6.0.1"
   val testInterfaceVer = "1.0"
 
   val junitPlatformLauncher = "org.junit.platform" % "junit-platform-launcher" % junitVersion
